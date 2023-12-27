53013936e5bd71b3f552c076b84830d637ba35be
unknown <inoublileith6@gmail.com>
2023-12-27 10:16:42 +0100

    27/12/2023

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {cors :true});
-
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
- 
-  });
-  await app.listen(3000);
+  app.enableCors();
+
+  await app.listen(process.env.PORT || 3000); 
 }
+
 bootstrap();
-  
