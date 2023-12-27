38be8336b0a5a78edd937018231046960972d2d1
Leith Inoubli <126243938+inoublileith@users.noreply.github.com>
2023-12-27 16:12:51 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   try {
     const app = await NestFactory.create(AppModule, { cors: true });
-
+    console.log("the app start $app");
     // Enable global validation pipe
     app.useGlobalPipes(new ValidationPipe());
 
@@ -14,10 +14,10 @@ async function bootstrap() {
 
     // Listen on the specified port or use a default (e.g., 3000)
     const port = process.env.PORT || 3000;
-
+    console.log(PORT);
     // Start the application
     await app.listen(3000);
-
+    
     console.log(`Application is running on: http://localhost:${port}`);
   } catch (error) {
     console.error('Error starting the NestJS application:', error);
