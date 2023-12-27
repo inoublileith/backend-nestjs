3105626a08c29a3f2f86e73b0125d767eb2b81f1
Leith Inoubli <126243938+inoublileith@users.noreply.github.com>
2023-12-27 11:35:37 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,20 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  try {
+    const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+    app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(process.env.PORT || 3000); 
+    app.enableCors();
+
+    const port = process.env.PORT || 3000;
+    await app.listen(port);
+
+    console.log(`Application is running on: http://localhost:${port}`);
+  } catch (error) {
+    console.error('Error starting the NestJS application:', error);
+  }
 }
 
 bootstrap();
